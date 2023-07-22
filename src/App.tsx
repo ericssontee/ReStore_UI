716640589b8f7b74bff81f6615b08e918d79b6b7
ericssontee <ericssontee.eb@gmail.com>
2023-07-22 19:09:54 +0800

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+const products = [
+  { name: "product1", price: 100.0 },
+  { name: "product2", price: 200.0 },
+  { name: "product2", price: 200.0 },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Re-Store</h1>
+      <ul>
+        {products.map((item, index) => {
+          return (
+            <li key={index}>
+              {item.name} - {item.price}
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
