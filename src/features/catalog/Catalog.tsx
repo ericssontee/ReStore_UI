59540fc7f8b7eab2dd443a233698b581642d7ad3
ericssontee <ericssontee.eb@gmail.com>
2023-08-05 15:16:08 +0800

    Add Search and Sort Functions

--- src/features/catalog/Catalog.tsx
+++ src/features/catalog/Catalog.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable array-callback-return */
+/* eslint-disable react/jsx-no-comment-textnodes */
+import { useEffect } from "react";
 // import { Product } from "../../app/models/product";
 import ProductList from "./ProductList";
 // import { useState, useEffect } from "react";
@@ -8,16 +11,34 @@ import {
   fetchFilters,
   fetchProductsAsync,
   productSelectors,
+  setProductParams,
 } from "./catalogSlice";
-import { useEffect } from "react";
+
+import {
+  Box,
+  Checkbox,
+  FormControlLabel,
+  FormGroup,
+  Grid,
+  Pagination,
+  Paper,
+  Typography,
+} from "@mui/material";
+import ProductSearch from "./ProductSearch";
+import RadionButtonGroup from "../../app/components/RadioButtonGroup";
+
+const sortOptions = [
+  { value: "name", label: "Alphabetical" },
+  { value: "priceDesc", label: "Price - Hight to low" },
+  { value: "price", label: "Price - Low to high" },
+];
 
 export default function Catalog() {
   // const [products, setProducts] = useState<Product[]>([]);
   // const [loading, setLoading] = useState(true);
   const products = useAppSelector(productSelectors.selectAll);
-  const { productsLoaded, status, filtersLoaded } = useAppSelector(
-    (state) => state.catalog
-  );
+  const { productsLoaded, status, filtersLoaded, brands, types, productParams } =
+    useAppSelector((state) => state.catalog);
   const dispatch = useAppDispatch();
 
   // useEffect(() => {
@@ -39,8 +60,51 @@ export default function Catalog() {
     return <LoadingComponent message="Loading products..." />;
 
   return (
-    <>
-      <ProductList products={products} />
-    </>
+    <Grid container spacing={4}>
+      <Grid item xs={3}>
+        <Paper sx={{ mb: 2 }}>
+          <ProductSearch />
+        </Paper>
+        <Paper sx={{ mb: 2, p: 2 }}>
+          <RadionButtonGroup 
+            selectedValue={productParams.orderBy}
+            options={sortOptions}
+            onChange={(e) => dispatch(setProductParams({orderBy: e.target.value}))}
+          />
+        </Paper>
+        <Paper sx={{ mb: 2, p: 2 }}>
+          <FormGroup>
+            {brands.map((brand) => (
+              <FormControlLabel
+                control={<Checkbox />}
+                label={brand}
+                key={brand}
+              />
+            ))}
+          </FormGroup>
+        </Paper>
+        <Paper sx={{ mb: 2, p: 2 }}>
+          <FormGroup>
+            {types.map((type) => (
+              <FormControlLabel
+                control={<Checkbox />}
+                label={type}
+                key={type}
+              />
+            ))}
+          </FormGroup>
+        </Paper>
+      </Grid>
+      <Grid item xs={9}>
+        <ProductList products={products} />
+      </Grid>
+      <Grid item xs={3} />
+      <Grid item xs={9}>
+        <Box display="flex" justifyContent="space-between" alignItems="center">
+          <Typography>Displaying 1-6 of 20 items</Typography>
+          <Pagination color='secondary' size="large" count={10} page={2}/>
+        </Box>
+      </Grid>
+    </Grid>
   );
 }
